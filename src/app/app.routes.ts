82eb7dfebeca0a0fa82461270d1814bda4b716f4
Timqooo <malinkaobecna@gmail.com>
2024-11-21 08:42:27 +0100

    routing, services, observable

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { LibraryComponent } from './components/library/library/library.component
 import { CartComponent } from './components/library/cart/cart.component';
 
 export const routes: Routes = [
-    { path: 'library', component: LibraryComponent},
+    { path: '', component: LibraryComponent},
     { path: 'cart', component: CartComponent}
 ];
